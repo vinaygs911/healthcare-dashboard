6c2d50e628e4d1fdc4135e07e70bf2ee64c7f71b
GS Vinay <vinay.g_s@siemens-healthineers.com>
2025-01-29 23:18:50 +0530

    Corrected app component error

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,12 +8,14 @@ import { AppComponent } from './app.component';
 import { PatientListComponent } from './components/patient-list/patient-list.component';
 import { patientReducer } from './store/patient.reducer';
 import { PatientEffects } from './store/patient.effects';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [AppComponent, PatientListComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
+    RouterModule.forRoot([]),
     StoreModule.forRoot({ patient: patientReducer }),
     EffectsModule.forRoot([PatientEffects])
   ],
